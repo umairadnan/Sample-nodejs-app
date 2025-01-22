da195edf79eeaef254f79b9baa1dc19783deb168
Umair Adnan Shafi <97875076+umairadnan@users.noreply.github.com>
2025-01-22 15:54:51 +0500

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var express = require('express'),
     crypto = require('crypto'),
     flash = require('connect-flash'),
     mongoose = require('mongoose'),
-    morgan       = require('morgan');
+    morgan = require('morgan');
 
 var handlebars = require('express-handlebars').create({
     layoutsDir: "views/layouts",
@@ -16,15 +16,27 @@ var handlebars = require('express-handlebars').create({
     defaultLayout: 'main'
 });
 
-//Intialise express framework
+// Initialize express framework
 var app = express();
 
 app.use(morgan('dev')); // log every request to the console
-mongoose.connect('mongodb://127.0.0.1:27017/samplenodejsapp')
+
+// Use environment variable for MongoDB URI, defaulting to localhost if not defined
+var mongoURI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/samplenodejsapp';
+
+// Connect to MongoDB
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('MongoDB connected successfully');
+}).catch((err) => {
+    console.log('Error connecting to MongoDB:', err);
+});
 
 require('./src/middleware/passport')(passport);
 
-app.use(cookieParser('SampleNode.jsApp')); //read cookies
+app.use(cookieParser('SampleNode.jsApp')); // read cookies
 app.use(cookieSession({
     keys: ['key1', 'key2']
 }));
@@ -34,13 +46,13 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.set('views', __dirname + '/views'); // set template directory
-app.set('view engine', 'handlebars'); //set view engine to handlebars
+app.set('view engine', 'handlebars'); // set view engine to handlebars
 app.engine('handlebars', handlebars.engine);
 app.use(expressValidator());
 app.use(passport.initialize());
-app.use(passport.session()); //persistant login sessions
-app.use(serveStatic('./public')); //serve static files from public directory
-app.use(flash()); //used to display flash messages from sessions
+app.use(passport.session()); // persistent login sessions
+app.use(serveStatic('./public')); // serve static files from public directory
+app.use(flash()); // used to display flash messages from sessions
 
 require('./routes')(app, passport); // include routes
 app.listen(process.env.PORT || 3000);
